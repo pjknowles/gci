449b3ca3e067bef39164bdc745ca4ba54c91c7e2
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2014-01-30 05:38:41 +0000

    Tidy Wavefunction

--- gciWavefunction.cpp
+++ gciWavefunction.cpp
@@ -1,27 +1,2 @@
 #include "gciWavefunction.h"
 
-Wavefunction::Wavefunction(unsigned int Nelec, int MS2)
-{
-    nelec = Nelec;
-    ms2 = MS2;
-}
-
-void Wavefunction::setMS2(int MS2)
-{
-    ms2 = MS2;
-}
-
-void Wavefunction::setNelec(unsigned int Nelec)
-{
-    nelec = Nelec;
-}
-
-unsigned int Wavefunction::Nelec()
-{
-    return nelec;
-}
-
-int Wavefunction::MS2()
-{
-    return ms2;
-}
--- gciWavefunction.h
+++ gciWavefunction.h
@@ -2,29 +2,17 @@
 #define GCIWAVEFUNCTION_H
 #include "gci.h"
 #include "gciHamiltonian.h"
+#include "gciState.h"
 
 namespace gci {
 /*!
  * \brief The Wavefunction class, which holds a configuration expansion in the tensor space defined by a hamiltonian
  */
-class Wavefunction
+class Wavefunction : public State
 {
 public:
-    /*!
-     * \brief Construct a Wavefunction
-     * \param Nelec the number of electrons
-     * \param MS2 Sz quantum number * 2
-     */
-    Wavefunction(unsigned int Nelec=0,int MS2=0);
-    void setNelec(unsigned int Nelec); ///< set the number of electrons
-    void setMS2(int MS2); ///< set the Sz quantum number
-    unsigned int Nelec(); ///< report the number of electrons
-    int MS2();///< report the Sz quantum number * 2
     double* buffer; ///< buffer to hold coefficients describing the object
 private:
-    Hamiltonian hamiltonian; ///< \brief the underlying second-quantized hamiltonian operator
-    unsigned int nelec; ///< number of electrons
-    int ms2; ///< twice the spin quantum number, ie multiplicity minus one
 };
 }
 using namespace gci;
