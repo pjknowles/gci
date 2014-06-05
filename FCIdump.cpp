1cdd5001fa057a49b6fce034d6c70fd5380905c5
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2014-06-05 11:04:20 +0100

    Reimplement FCIdump::parameter

--- FCIdump.cpp
+++ FCIdump.cpp
@@ -1,53 +1,66 @@
 #include "FCIdump.h"
 #include <iostream>
+#include <fstream>
+#include <istream>
+#include <sstream>
+#include "gci.h" // for xout
 
 FCIdump::FCIdump(std::string filename)
 {
   fileName = filename;
-}
-
-#include "gciState.h"
-#include <fstream>
-#include <istream>
-#include <sstream>
-
-
-
-std::vector<int> FCIdump::parameter(std::string key, std::vector<int> def) { // dirty sucking in from FCIDUMP namelist
-  std::vector<int> answer;
   std::ifstream s;
   s.open(fileName.c_str());
   if ( (s.rdstate() & std::ifstream::failbit ) != 0 ) {
     std::cerr << "Error opening " << fileName <<std::endl;
     throw "FCIDUMP::parameter file missing";
   }
+  // cache the namelist data
   std::string ss;
-  std::string keyin("");// the current keyword
-  while (s >> ss && ss != "&END" && ss != "/") {
-    while (ss.size()) {
-      // gobble up leading spaces
-      while (ss.size() && ss[0]==' ') ss.erase(0,1);
-      // gobble the next thing up to equals, comma, end of line
-      size_t iend = ss.find_first_of("=,"); if (iend==std::string::npos) iend=ss.size();
-      std::string thisword = ss.substr(0,iend);
-      ss=ss.substr(iend >= ss.size()-1 ? ss.size() : iend+1 );
-      //            xout << "thisword=" <<thisword <<"; new ss=" <<ss <<std::endl;
-      if (isdigit(thisword[0])) {
-        int i;
-        std::istringstream b(thisword);
-        b >>i;
-        //                xout <<"first is digit =" <<i <<endl;
-        if (key==keyin) answer.push_back(i);
-        //                if (key==keyin) xout << "taking this for key="<<key<<endl;
-      } else {
-        keyin=thisword;
-        //                xout <<"keyin="<<keyin<<endl;
-      }
-    }
-  }
-  s.close();
+  s >> ss ; namelistData=","; // forget the first word of namelist
+  while (s >> ss && ss != "&END" && ss != "/")
+    namelistData.append(ss);
+  namelistData.append(",DUMMY_KEY=,"); // dummy entry at end to simplify parsing
+//  xout <<"namelistData=" <<namelistData <<std::endl;
+}
 
-  //    xout << "parameter "<<key<<"="; for (std::vector<int>::iterator s=answer.begin(); s < answer.end(); s++) xout <<*s ; xout <<std::endl;
-  if (answer.size()==0) return def;
+#include "gciState.h"
+
+
+
+std::vector<int> FCIdump::parameter(std::string key, std::vector<int> def) { // dirty sucking in from FCIDUMP namelist
+  std::vector<int> answer;
+  std::vector<std::string> strings = parameter(key,std::vector<std::string>(1," "));
+//  xout <<"parameter "<<key<<std::endl;
+//  xout << "parameter "<<key<<"="; for (std::vector<std::string>::iterator s=strings.begin(); s < strings.end(); s++) xout <<*s ; xout <<std::endl;
+  if (strings == std::vector<std::string>(1," ")) return def;
+  for (std::vector<std::string>::const_iterator s1=strings.begin(); s1!= strings.end(); s1++) {
+    int i;
+    std::istringstream b(*s1);
+    b >>i;
+//    xout <<"s1="<<*s1<<"; i="<<i<<std::endl;
+    answer.push_back(i);
+  }
+//  xout << "parameter "<<key<<"="; for (std::vector<int>::iterator s=answer.begin(); s < answer.end(); s++) xout <<*s ; xout <<std::endl;
+  return answer;
+}
+
+std::vector<std::string> FCIdump::parameter(std::string key, std::vector<std::string> def) { // dirty sucking in from FCIDUMP namelist
+  std::vector<std::string> answer;
+//  xout <<"search for "<<","<<key<<"="<<std::endl;
+  size_t pos = namelistData.find(","+key+"=");
+  if (pos == std::string::npos) return def;
+  pos=namelistData.find("=",pos)+1;
+  size_t pose=namelistData.find("=",pos);
+  pose=namelistData.find_last_of(",",pose)+1;
+//  xout << "string parameter key="<<key<<"; values='"<<namelistData.substr(pos,pose-pos)<<"'"<<std::endl;
+//  xout << "pos="<<pos<<"; pose="<<pose<<std::endl;
+  for (size_t posNext=pos; posNext < pose; pos=posNext) {
+    posNext=namelistData.find(",",pos)+1;
+    answer.push_back(namelistData.substr(pos,posNext-pos-1));
+  }
+//  xout <<"answer";
+//  for (std::vector<std::string>::const_iterator s=answer.begin(); s != answer.end(); s++)
+//    xout <<":"<<*s;
+//  xout <<std::endl;
   return answer;
 }
--- FCIdump.h
+++ FCIdump.h
@@ -16,7 +16,7 @@ public:
   FCIdump(std::string filename="FCIDUMP");
 
   /*!
-     * \brief parameter Obtain a namelist parameter from the FCIDUMP data.
+     * \brief parameter Obtain an integer namelist parameter from the FCIDUMP data.
      * \param key The name of the parameter
      * \param def Default value if the parameter is not found.
      * \return  The result as a vector of integers.
@@ -24,9 +24,27 @@ public:
   std::vector<int> parameter(std::string key, std::vector<int> def=std::vector<int>(1,0));
 
   /*!
+     * \brief parameter Obtain a real namelist parameter from the FCIDUMP data.
+     * \param key The name of the parameter
+     * \param def Default value if the parameter is not found.
+     * \return  The result as a vector of integers.
+     */
+  std::vector<double> parameter(std::string key, std::vector<double> def);
+
+  /*!
+     * \brief parameter Obtain a string namelist parameter from the FCIDUMP data.
+     * \param key The name of the parameter
+     * \param def Default value if the parameter is not found.
+     * \return  The result as a vector of integers.
+     */
+  std::vector<std::string> parameter(std::string key, std::vector<std::string> def);
+
+   /*!
      * \brief fileName The file containing the FCIDUMP data
      */
   std::string fileName;
+private:
+  std::string namelistData;
 };
 
 #endif // FCIDUMP_H
