5c180c03af61e777c1c6745eeb3e02c5c8c9c9b2
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2014-09-18 10:25:44 -0400

    Profiler merge from Molpro

--- ProfilerC.h
+++ ProfilerC.h
@@ -4,8 +4,9 @@ extern "C" {
 void* profilerNew(char* name);
 void profilerReset(void* profiler, char* name);
 void profilerStart(void* profiler, char* name);
-void profilerStop(void* profiler, char* name);
+void profilerStop(void* profiler, char* name, long operations=0);
 char* profilerStr(void* profiler);
+void profilerStrSubroutine(void*profiler, char* result, int maxResult);
 }
 
 #endif // PROFILERC_H
