be590177aacc70a517812707e82351e82f0d8a71
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2014-09-18 11:54:35 -0400

    tidy task scheduling

--- gci.h
+++ gci.h
@@ -13,9 +13,9 @@
 #endif
 
 #ifdef GCI_PARALLEL
-  extern "C" {
+extern "C" {
 #include "ppidd_c.h"
-  }
+}
 #endif
 
 
@@ -25,29 +25,32 @@ namespace gci {
 extern Profiler profiler; // global profiler
 
 extern int64_t parallel_rank, parallel_size;
+
+// shared counter
 extern int64_t __nextval_counter;
-  inline long nextval(int64_t option=parallel_size){
+inline long nextval(int64_t option=parallel_size){
 #ifdef GCI_PARALLEL
-      int64_t value; PPIDD_Nxtval(&option,&value); //xout <<std::endl<<"@nextval("<<option<<",rank="<<parallel_rank<<")="<<value<<std::endl;
-      return value;
+  int64_t value; PPIDD_Nxtval(&option,&value); //xout <<std::endl<<"@nextval("<<option<<",rank="<<parallel_rank<<")="<<value<<std::endl;
+  return value;
 #else
-      if (option < 0) __nextval_counter=-1;
-      return ++__nextval_counter;
+  if (option < 0) __nextval_counter=-1;
+  return ++__nextval_counter;
 #endif
 }
 
-  extern int64_t __task_granularity, __task, __my_first_task;
-  inline void initask(int64_t granularity) {
-    __task_granularity = granularity;
-    nextval(-parallel_size);
-    __task=0;
-    __my_first_task=-granularity-1;
-  }
-  inline bool mytask() {
-    if (__my_first_task+__task_granularity <= __task)
-      __my_first_task = nextval()*__task_granularity;
-    return (__task++ >= __my_first_task && __task <= __my_first_task+__task_granularity) ;
-  }
+// task scheduler
+extern int64_t __task_granularity, __task, __my_first_task;
+inline void initask(int64_t granularity) {
+  __task_granularity = granularity;
+  nextval(-parallel_size);
+  __task=0;
+  __my_first_task=-granularity-1;
+}
+inline bool mytask() {
+  if (__my_first_task+__task_granularity <= __task)
+    __my_first_task = nextval()*__task_granularity;
+  return (__task++ >= __my_first_task && __task <= __my_first_task+__task_granularity) ;
+}
 
 }
 
