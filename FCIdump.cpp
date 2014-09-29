db5b5912eeeb1545623eea22596d96691eea817b
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2014-09-29 13:02:00 +0100

    Bug 4531: no mainpage for FCIdump documentation

--- FCIdump.cpp
+++ FCIdump.cpp
@@ -3,9 +3,15 @@
 #include <fstream>
 #include <istream>
 #include <sstream>
-#include "gci.h" // for xout
+#define xout std::cout
 
-FCIdump::FCIdump(std::string filename)
+FCIdump::FCIdump()
+{
+  _fileName="";
+  namelistData=",";
+}
+
+FCIdump::FCIdump(const std::string filename)
 {
   _fileName = filename;
   std::ifstream s;
@@ -23,29 +29,28 @@ FCIdump::FCIdump(std::string filename)
 //  xout <<"namelistData=" <<namelistData <<std::endl;
 }
 
+FCIdump::FCIdump(const std::vector<char> bytestream)
+{
+
+}
+
 std::string FCIdump::fileName()
 {
   return _fileName;
 }
-
-#include "gciState.h"
 
 
 
 std::vector<int> FCIdump::parameter(std::string key, std::vector<int> def) { // dirty sucking in from FCIDUMP namelist
   std::vector<int> answer;
   std::vector<std::string> strings = parameter(key,std::vector<std::string>(1," "));
-//  xout <<"parameter "<<key<<std::endl;
-//  xout << "parameter "<<key<<"="; for (std::vector<std::string>::iterator s=strings.begin(); s < strings.end(); s++) xout <<*s ; xout <<std::endl;
   if (strings == std::vector<std::string>(1," ")) return def;
   for (std::vector<std::string>::const_iterator s1=strings.begin(); s1!= strings.end(); s1++) {
     int i;
     std::istringstream b(*s1);
     b >>i;
-//    xout <<"s1="<<*s1<<"; i="<<i<<std::endl;
     answer.push_back(i);
   }
-//  xout << "parameter "<<key<<"="; for (std::vector<int>::iterator s=answer.begin(); s < answer.end(); s++) xout <<*s ; xout <<std::endl;
   return answer;
 }
 
@@ -53,51 +58,70 @@ std::vector<int> FCIdump::parameter(std::string key, std::vector<int> def) { // 
 std::vector<double> FCIdump::parameter(std::string key, std::vector<double> def) { // dirty sucking in from FCIDUMP namelist
   std::vector<double> answer;
   std::vector<std::string> strings = parameter(key,std::vector<std::string>(1," "));
-//  xout <<"parameter "<<key<<std::endl;
-//  xout << "parameter "<<key<<"="; for (std::vector<std::string>::iterator s=strings.begin(); s < strings.end(); s++) xout <<*s ; xout <<std::endl;
   if (strings == std::vector<std::string>(1," ")) return def;
   for (std::vector<std::string>::const_iterator s1=strings.begin(); s1!= strings.end(); s1++) {
     double i;
     std::istringstream b(*s1);
     b >>i;
-//    xout <<"s1="<<*s1<<"; i="<<i<<std::endl;
     answer.push_back(i);
   }
-//  xout << "parameter "<<key<<"="; for (std::vector<double>::iterator s=answer.begin(); s < answer.end(); s++) xout <<*s ; xout <<std::endl;
   return answer;
 }
 
 std::vector<std::string> FCIdump::parameter(std::string key, std::vector<std::string> def) { // dirty sucking in from FCIDUMP namelist
   std::vector<std::string> answer;
-//  xout <<"search for "<<","<<key<<"="<<std::endl;
   size_t pos = namelistData.find(","+key+"=");
   if (pos == std::string::npos) return def;
   pos=namelistData.find("=",pos)+1;
   size_t pose=namelistData.find("=",pos);
   pose=namelistData.find_last_of(",",pose)+1;
-//  xout << "string parameter key="<<key<<"; values='"<<namelistData.substr(pos,pose-pos)<<"'"<<std::endl;
-//  xout << "pos="<<pos<<"; pose="<<pose<<std::endl;
   for (size_t posNext=pos; posNext < pose; pos=posNext) {
     posNext=namelistData.find(",",pos)+1;
     answer.push_back(namelistData.substr(pos,posNext-pos-1));
   }
-//  xout <<"answer";
-//  for (std::vector<std::string>::const_iterator s=answer.begin(); s != answer.end(); s++)
-//    xout <<":"<<*s;
-//  xout <<std::endl;
   return answer;
 }
 
 void FCIdump::addParameter(const std::string& key, const std::vector<std::string>& values)
 {
 //  xout << "FCIdump::addParameter namelistData originally "<<namelistData<<std::endl;
-//  for (std::string::const_reverse_iterator s=values.rbegin();
-//       s != values.rend(); s++)
   namelistData.erase(0,1);
-  for (int i=values.size()-1; i>-1; i--)
-    namelistData.insert(0,values[i]+",");
+  for (std::vector<std::string>::const_reverse_iterator s=values.rbegin(); s != values.rend(); s++)
+    namelistData.insert(0,(*s)+",");
   namelistData.insert(0,","+key+"=");
 //  xout << "FCIdump::addParameter namelistData set to "<<namelistData<<std::endl;
+}
+void FCIdump::addParameter(const std::string& key, const std::vector<int>& values)
+{
+  std::vector<std::string> valuess;
+  for (std::vector<int>::const_iterator v=values.begin(); v!=values.end(); v++) {
+    std::ostringstream ss;
+    ss << *v;
+    valuess.push_back(ss.str());
+  }
+  addParameter(key,valuess);
+}
+void FCIdump::addParameter(const std::string& key, const std::vector<double>& values)
+{
+  std::vector<std::string> valuess;
+  for (std::vector<double>::const_iterator v=values.begin(); v!=values.end(); v++) {
+    std::ostringstream ss;
+    ss << *v;
+    valuess.push_back(ss.str());
+  }
+  addParameter(key,valuess);
+}
+void FCIdump::addParameter(const std::string &key, const std::string &value)
+{
+  addParameter(key,std::vector<std::string>(1,value));
+}
+void FCIdump::addParameter(const std::string &key, const int &value)
+{
+  addParameter(key,std::vector<int>(1,value));
+}
+void FCIdump::addParameter(const std::string &key, const double &value)
+{
+  addParameter(key,std::vector<double>(1,value));
 }
 
 void FCIdump::rewind()
@@ -116,6 +140,71 @@ void FCIdump::rewind()
   currentState = states.begin();
 }
 
+#include <iomanip>
+bool FCIdump::write(std::string filename, fileType type, bool integrals)
+{
+  outputStream.open(filename.c_str());
+  if ( (outputStream.rdstate() & std::ifstream::failbit ) != 0 ) {
+    std::cout << "FCIdump::write failed to open "<<filename<<std::endl;
+    outputStream.close();
+    return false;
+  }
+  outputStream << "&FCI" << std::endl;
+  size_t pos=0;
+  while ( (pos = namelistData.find('=',pos)) != std::string::npos){
+    std::string key=namelistData.substr(0,pos);
+    size_t pose=namelistData.find("=",pos+1);
+    pose=namelistData.find_last_of(",",pose)+1;
+    while (namelistData.substr(pose-2,1) == ",") pose--;
+    pos=namelistData.find_last_of(",",pos)+1;
+    if (namelistData.substr(pos,pose-pos).find("DUMMY_KEY") == std::string::npos)
+      outputStream << " " << namelistData.substr(pos,pose-pos) << std::endl;
+    pos=pose;
+  }
+  outputStream<< "/"<<std::endl;
+  outputStream<<std::scientific<<std::setprecision(15);
+  rewind();
+  if (integrals) {
+  int i,j,k,l;
+  double value;
+  while (nextIntegral(i,j,k,l,value) != endOfFile)
+    writeIntegral(i,j,k,l,value);
+  }
+  outputStream.close();
+  return true;
+}
+
+std::vector<char> FCIdump::bytestream(bool integrals)
+{
+  std::vector<char> result;
+  for (std::string::const_iterator s=namelistData.begin(); s!=namelistData.end(); s++)
+    result.push_back(*s);
+  if (integrals) {
+    rewind();
+    int i,j,k,l;
+    union {
+      struct {
+        int16_t labels[4];
+        double value;
+      } s;
+      char buf[16];
+    } u;
+    while (nextIntegral(i,j,k,l,u.s.value)!=endOfFile) {
+      u.s.labels[0]=i;
+      u.s.labels[1]=j;
+      u.s.labels[2]=k;
+      u.s.labels[3]=l;
+    }
+    for(i=0; i<16; i++) result.push_back(u.buf[i]);
+  }
+  return result;
+}
+
+void FCIdump::writeIntegral(int i, int j, int k, int l, double value)
+{
+    outputStream<<value<<" "<< i<<" "<<j<<" "<<k<<" "<<l<<std::endl;
+}
+
 FCIdump::integralType FCIdump::nextIntegral(int &i, int &j, int &k, int &l, double &value)
 {
   integralType result = *currentState;
@@ -128,15 +217,100 @@ FCIdump::integralType FCIdump::nextIntegral(int &i, int &j, int &k, int &l, doub
   }
   // following is tricky stuff reflecting historical structure of UHF and RHF FCIdump files
   if (i == 0) {
+//    std::cout << "zero read uhf="<<uhf<<", *currentState="<<*currentState<<std::endl;
     if (uhf && *currentState != I0) {
       result = endOfRecord;
+      currentState++;
+//      std::cout << "end of Record signalled; *currentState="<<*currentState<<std::endl;
     }
     else {
+//      std::cout << "real scalar signalled"<<std::endl;
       result = I0;
-      currentState++;
     }
   }
-  if (k == 0 && *currentState == I2aa) result=*(++currentState);
+  else if (k == 0 && (*currentState != FCIdump::I1a && *currentState != FCIdump::I1b)) {
+//    std::cout << "special state switch to "<<*(currentState+1)<<std::endl;
+    result=(*currentState==FCIdump::I2aa)?FCIdump::I1a:FCIdump::endOfRecord; ++currentState;
+  }
+  //xout << "i,j,k,l,value "<<i<<","<<j<<","<<k<<","<<l<<","<<value<<", result="<<result<<std::endl;
   return result;
 }
 
+FCIdump* dump;
+
+void FCIdumpInitialise(char* filename)
+{
+//  printf("Initialise FCIDUMP from file %s\n",filename);
+  dump = new FCIdump(std::string(filename));
+}
+
+#include <string.h>
+void FCIdumpParameterS(char* key, char *value)
+{
+  std::vector<std::string> vals(1);
+  vals[0]=value;
+  vals = dump->parameter(std::string(key), vals);
+  if (vals.size()>0) value=strdup(vals[0].c_str());
+}
+
+void FCIdumpParameterI(char* key, int* values, int n)
+{
+  std::vector<int> vals(n);
+  for (size_t i=0; i<(size_t)n; i++) vals[i]=values[i];
+  vals = dump->parameter(std::string(key), vals);
+  for (size_t i=0; i<vals.size() && i < (size_t)n; i++)
+    values[i]=vals[i];
+}
+
+void FCIdumpParameterF(char* key, double* values, int n)
+{
+  std::vector<double> vals(n);
+  for (size_t i=0; i<(size_t)n; i++) vals[i]=values[i];
+  vals = dump->parameter(std::string(key), vals);
+  for (size_t i=0; i<vals.size() && i < (size_t)n; i++)
+    values[i]=vals[i];
+}
+
+void FCIdumpRewind()
+{
+  dump->rewind();
+}
+
+int FCIdumpNextIntegral(int* i, int* j, int* k, int* l, double* value)
+{
+  int ii, jj, kk, ll;
+  double vv;
+  FCIdump::integralType type = dump->nextIntegral(ii, jj, kk, ll, vv);
+  *i = ii; *j = jj; *k = kk; *l = ll; *value = vv;
+  return (int) type;
+}
+
+void FCIdumpAddParameterS(char *key, char *value)
+{
+  std::string keys(key);
+  std::vector<std::string> valuess(1,value);
+  dump->addParameter(keys,valuess);
+}
+
+void FCIdumpAddParameterI(char *key, int values[], int n)
+{
+  std::string keys(key);
+  std::vector<int> valuess;
+  for (size_t i=0; i<(size_t)n; i++)
+    valuess.push_back(values[i]);
+  dump->addParameter(keys,valuess);
+}
+
+void FCIdumpAddParameterF(char *key, double values[], int n)
+{
+  std::string keys(key);
+  std::vector<double> valuess;
+  for (size_t i=0; i<(size_t)n; i++)
+    valuess.push_back(values[i]);
+  dump->addParameter(keys,valuess);
+}
+
+int FCIdumpWrite(char* filename, int type)
+{
+  return dump->write(std::string(filename),(FCIdump::fileType) type) ? 1 : 0 ;
+}
