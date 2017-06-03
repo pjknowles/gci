c8e9395b2f7c58d13951df81bdd642b3fb434e3e
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2017-06-03 15:56:22 +0100

    Operator loads

--- src/gciOperator.cpp
+++ src/gciOperator.cpp
@@ -3,7 +3,7 @@
 
 gci::Operator gci::Operator::construct(const FCIdump &dump)
 {
-  int verbosity=0;
+  int verbosity=1;
   std::vector<int> orbital_symmetries = dump.parameter("ORBSYM");
   dim_t dim(8);
   for (auto& s : orbital_symmetries)
@@ -11,19 +11,24 @@ gci::Operator gci::Operator::construct(const FCIdump &dump)
 
   Operator result(dims_t(4,dim),2,dump.parameter("IUHF")[0]>0,{1,1},{-1,-1},0,"Hamiltonian");
   result.m_orbital_symmetries = orbital_symmetries;
-  for (auto i=0; i<orbital_symmetries.size(); i++) {
-      xout << "i="<<i+1<<", symmetry="<<orbital_symmetries[i]<<", offset="<<result.offset(i+1)<<std::endl;;
-    }
+//  for (auto i=0; i<orbital_symmetries.size(); i++)
+//      xout << "i="<<i+1<<", symmetry="<<orbital_symmetries[i]<<", offset="<<result.offset(i+1)<<std::endl;;
 
   dump.rewind();
   double value;
   FCIdump::integralType type;
   int i,j,k,l;
-  SMat& integrals_a = result.O1(true);
-  SMat& integrals_b = result.O1(false);
+  SMat& integrals_a = result.O1(true); integrals_a.assign(0);
+  SMat& integrals_b = result.O1(false);integrals_b.assign(0);
   SMatMat& integrals_aa = result.O2(true,true);
   SMatMat& integrals_ab = result.O2(true,false);
   SMatMat& integrals_bb = result.O2(false,false);
+  if (verbosity>0) {
+      xout << "integral addresses "<< &integrals_a<<" "<<&integrals_b<<std::endl;
+      xout << "integral addresses "<< &integrals_a.block(0)[0]<<" "<<&integrals_b.block(0)[0]<<std::endl;
+      xout << "integral addresses "<< &integrals_aa<<" "<<&integrals_ab<<" "<<&integrals_bb<<std::endl;
+      xout << "integral sizes "<< integrals_aa.size()<<" "<<integrals_ab.size()<<" "<<integrals_bb.size()<<std::endl;
+    }
   while ((type=dump.nextIntegral(i,j,k,l,value))!=FCIdump::endOfFile) {
       auto oi = result.offset(i);
       auto oj = result.offset(j);
@@ -33,22 +38,25 @@ gci::Operator gci::Operator::construct(const FCIdump &dump)
       auto sj = j<1 ? 0 : orbital_symmetries[j-1];
       auto sk = k<1 ? 0 : orbital_symmetries[k-1];
       auto sl = l<1 ? 0 : orbital_symmetries[l-1];
-      xout << "ijkl "<<i<<j<<k<<l<<std::endl;
-      xout << "s: ijkl "<<si<<sj<<sk<<sl<<std::endl;
-      xout << "o: ijkl "<<oi<<oj<<ok<<ol<<std::endl;
+//      xout << "ijkl "<<i<<j<<k<<l<<std::endl;
+//      xout << "s: ijkl "<<si<<sj<<sk<<sl<<std::endl;
+//      xout << "o: ijkl "<<oi<<oj<<ok<<ol<<std::endl;
       if (si>sj || (si==sj && oi < oj)) { std::swap(oi,oj); std::swap(si,sj);}
       if (sk>sl || (sk==sl && ok < ol)) { std::swap(ok,ol); std::swap(sk,sl);}
       unsigned int sij = si^sj;
-      xout << "s: ijkl "<<si<<sj<<sk<<sl<<std::endl;
-      xout << "o: ijkl "<<oi<<oj<<ok<<ol<<std::endl;
+//      xout << "s: ijkl "<<si<<sj<<sk<<sl<<std::endl;
+//      xout << "o: ijkl "<<oi<<oj<<ok<<ol<<std::endl;
 
       if (type == FCIdump::I2aa) {
           if (verbosity>2) xout << "aa("<< i << j <<"|"<< k << l <<") = " << value <<std::endl;
           if (verbosity>2) xout << "aa("<< k << l <<"|"<< i << j <<") = " << value <<std::endl;
-          if (sij) xout << "block "<<integrals_aa.smat(sij,si,oi,oj)->blockM(sk)<<std::endl;
-          sij ? integrals_aa.smat(sij,si,oi,oj)->blockM(sk)(ok,ol) : integrals_aa.smat(sij,si,oi,oj)->block(sk)[ok*(ok+1)/2+ol] = value;
-          if (sij) xout << "block "<<integrals_aa.smat(sij,si,ok,ol)->blockM(sk)<<std::endl;
-          sij ? integrals_aa.smat(sij,sk,ok,ol)->blockM(si)(oi,oj) : integrals_aa.smat(sij,sk,ok,ol)->block(si)[oi*(oi+1)/2+oj] = value;
+          (sij ? integrals_aa.smat(sij,si,oi,oj)->blockM(sk)(ok,ol) : integrals_aa.smat(sij,si,oi,oj)->block(sk)[ok*(ok+1)/2+ol]) = value;
+          (sij ? integrals_aa.smat(sij,sk,ok,ol)->blockM(si)(oi,oj) : integrals_aa.smat(sij,sk,ok,ol)->block(si)[oi*(oi+1)/2+oj]) = value;
+          if (sij)
+           xout << "aa("<< i << j <<"|"<< k << l <<") = " << value
+                <<" "<< integrals_aa.smat(sij,si,oi,oj)->blockM(sk)(ok,ol)
+                <<" "<< &integrals_aa.smat(sij,si,oi,oj)->blockM(sk)(ok,ol)
+                <<std::endl;
         } else if (type == FCIdump::I2ab) {
           if (verbosity>2) xout << "ab("<< i << j <<"|"<< k << l <<") = " << value <<std::endl;
           sij ? integrals_ab.smat(sij,si,oi,oj)->blockM(sk)(ok,ol) : integrals_ab.smat(sij,si,oi,oj)->block(sk)[ok*(ok+1)/2+ol] = value;
@@ -66,6 +74,59 @@ gci::Operator gci::Operator::construct(const FCIdump &dump)
         } else if (type == FCIdump::I0)
         result.m_O0 = value;
     }
+  if (verbosity>0) xout << result << std::endl;
+  if (verbosity>-1) xout << "int1:\n" <<result.int1(1)<< std::endl;
+  if (verbosity>-1) xout << "intJ:\n" <<result.intJ(1,1)<< std::endl;
+  if (verbosity>-1) xout << "intK:\n" <<result.intK(1)<< std::endl;
 
+  return result;
+}
+
+Eigen::VectorXd gci::Operator::int1(int spin) const
+{
+  size_t basisSize = m_orbital_symmetries.size();
+  Eigen::VectorXd result(basisSize);
+  for (unsigned int i=0; i < basisSize; i++) {
+    result[i] = O1(spin>0).block(m_orbital_symmetries[i])[(offset(i+1)+1)*(offset(i+1)+2)/2-1];
+  }
+  return result;
+}
+
+
+Eigen::MatrixXd gci::Operator::intJ(int spini, int spinj) const
+{
+  if (spinj > spini) return intJ(spinj,spini).transpose();
+  size_t basisSize = m_orbital_symmetries.size();
+  Eigen::MatrixXd result(basisSize,basisSize);
+  for (unsigned int j=0; j < basisSize; j++) {
+    for (unsigned int i=0; i < basisSize; i++) {
+      auto si = m_orbital_symmetries[i];
+      auto sj = m_orbital_symmetries[j];
+      auto oi = offset(i+1);
+      auto oj = offset(j+1);
+      result(i,j) = O2(spini>0,spinj>0).smat(0,si,oi,oi)->block(sj)[(oj+2)*(oj+1)/2-1];
+    }
+  }
+  return result;
+}
+
+Eigen::MatrixXd gci::Operator::intK(int spin) const
+{
+  size_t basisSize = m_orbital_symmetries.size();
+  Eigen::MatrixXd result(basisSize,basisSize);
+  for (unsigned int j=0; j < basisSize; j++) {
+    for (unsigned int i=0; i < basisSize; i++) {
+      auto si = m_orbital_symmetries[i];
+      auto sj = m_orbital_symmetries[j];
+      auto oi = offset(i+1);
+      auto oj = offset(j+1);
+      result(i,j) =
+          ((si < sj) ?
+             O2(spin>0,spin>0).smat(si^sj,si,oi,oj)->blockM(si)(oi,oj)
+           :
+             O2(spin>0,spin>0).smat(si^sj,sj,oj,oi)->blockM(sj)(oj,oi)
+             );
+    }
+  }
   return result;
 }
--- src/gciOperator.h
+++ src/gciOperator.h
@@ -2,6 +2,7 @@
 #define GCIOPERATOR_H
 #include "Operator.h"
 #include "FCIdump.h"
+#include "Eigen/Dense"
 
 
 namespace gci {
@@ -19,38 +20,37 @@ namespace gci {
     static Operator construct(const FCIdump& dump);
 
     /*!
-     * \brief offset Return the number of orbitals of the same symmetry before the given one.
-     * \param i Absolute number (starting with 1) of the orbital.
-     * \return
-     */
-    size_t offset(unsigned int i) {
-      return i < 1 ? 0 : std::count(m_orbital_symmetries.begin(),m_orbital_symmetries.begin()+i-1,m_orbital_symmetries[i-1]);
-    }
-
-    /*!
        * \brief int1 Generate array of diagonal one-electron integrals
        * \param spin positive for alpha, negative for beta
        * \return one-dimensional array with h(i,i) at i-1
        */
-    std::vector<double> int1(int spin) const;
-
+    Eigen::VectorXd int1(int spin) const;
 
     /*!
        * \brief intJ Generate array of two-electron exchange integrals
        * \param spini positive for alpha, negative for beta, first index
        * \param spinj positive for alpha, negative for beta, second index
-       * \return one-dimensional array with (ii|jj) at i-1 + (j-1)*basisSize
+       * \return array with (ii|jj)
        */
-    std::vector<double> intJ(int spini, int spinj) const;
+    Eigen::MatrixXd intJ(int spini, int spinj) const;
     /*!
        * \brief intK Generate array of two-electron Coulomb integrals
        * \param spin positive for alpha, negative for beta
-       * \return one-dimensional array with (ij|ji) at i-1 + (j-1)*basisSize
+       * \return array with (ij|ji)
        */
-    std::vector<double> intK(int spin) const;
+    Eigen::MatrixXd intK(int spin) const;
 
   private:
   std::vector<int> m_orbital_symmetries;
+
+    /*!
+     * \brief offset Return the number of orbitals of the same symmetry before the given one.
+     * \param i Absolute number (starting with 1) of the orbital.
+     * \return
+     */
+    size_t offset(unsigned int i) const {
+      return i < 1 ? 0 : std::count(m_orbital_symmetries.begin(),m_orbital_symmetries.begin()+i-1,m_orbital_symmetries[i-1]);
+    }
   };
 
 }
