2392e86b8550e025fdc65afce5eb0824fc271571
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2017-06-06 21:03:51 +0100

    .

--- src/gciExcitationOperator.cpp
+++ src/gciExcitationOperator.cpp
@@ -1,7 +1,7 @@
 #include "gciExcitationOperator.h"
 
-ExcitationOperator::ExcitationOperator(OldOperator &hamiltonian_, std::vector<int> ranks_)
-{
-  this->ranks = ranks_;
-  this->hamiltonian = hamiltonian_;
-}
+//ExcitationOperator::ExcitationOperator(OldOperator &hamiltonian_, std::vector<int> ranks_)
+//{
+//  this->ranks = ranks_;
+//  this->hamiltonian = hamiltonian_;
+//}
