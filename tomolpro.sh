a33942bfe779a4f4f82da95a8d2f4f2236731357
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2017-12-18 13:58:21 +0000

    tomolpro

--- tomolpro.sh
+++ tomolpro.sh
@@ -7,7 +7,7 @@ fi
 echo "Synchronize to $1"
 
 rsync src/*.{h,cpp} $1/src/gci
-rsync submodules/memory/memory.h submodules/memory/bytestreamC* $1/src/global
+rsync submodules/memory/bytestreamC* $1/src/global
 rsync submodules/symmetry_matrix/*.{cpp,h} submodules/symmetry_matrix/SymmetryMatrixF.F90 $1/src/global
 rsync submodules/Profiler/Profiler* $1/src/global
 rsync submodules/FCIdump/FCIdump* $1/src/global
