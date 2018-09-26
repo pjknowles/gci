2c2f9d7e0139dcb6c45c08351943f165ef4b2bb2
Marat <SibaevM@cardiff.ac.uk>
2018-09-26 16:49:43 +0100

    MixedWavefunction: linear algebra routines implemented. Next operatorOnWavefunction.
    Signed-off-by: Marat <SibaevM@cardiff.ac.uk>

--- src/gciMixedWavefunction.cpp
+++ src/gciMixedWavefunction.cpp
@@ -1,68 +1,165 @@
 #include "gciMixedWavefunction.h"
 
+namespace gci {
 MixedWavefunction::MixedWavefunction(const State &state, int nMode, int nModal, int modeCoupling)
-        : nMode_(nMode), nModal_(nModal), modeCoupling_(modeCoupling) {
+        : m_nMode(nMode), m_nModal(nModal), m_modeCoupling(modeCoupling), m_elecWfnSize(0), m_dimension(0) {
     assert((modeCoupling == 1 || modeCoupling == 2) && "Current Hamiltonian assumes 1 or 2 mode coupling");
     // Zeroth order
-    wfn_.emplace_back(state);
+    m_wfn.emplace_back(state);
+    m_elecWfnSize = m_wfn[0].size();
+    m_dimension = m_elecWfnSize;
     // First order coupling
-    for (int iMode = 0; iMode < nMode_; ++iMode) {
-        for (int iModal = 0; iModal < nModal_; ++iModal) {
-            wfn_.emplace_back(state);
+    for (int iMode = 0; iMode < m_nMode; ++iMode) {
+        for (int iModal = 0; iModal < m_nModal; ++iModal) {
+            m_wfn.emplace_back(state);
         }
     }
+    m_dimension += m_nMode * m_nModal * m_elecWfnSize;
     // Second order coupling
-    if (modeCoupling_ > 1) {
+    if (m_modeCoupling > 1) {
         throw std::logic_error("modeCoupling > 1 is not yet implemented");
     }
 }
 
 bool MixedWavefunction::compatible(const MixedWavefunction &w2) const {
-    bool b_size = (wfn_.size() == w2.wfn_.size());
+    bool b_size = (m_wfn.size() == w2.m_wfn.size());
     if (!b_size) return b_size;
     bool b_electronic_wfn = true;
-    for (int i = 0; i < wfn_.size(); ++i) {
-        b_electronic_wfn = b_electronic_wfn && wfn_[i].compatible(w2.wfn_[i]);
+    for (int i = 0; i < m_wfn.size(); ++i) {
+        b_electronic_wfn = b_electronic_wfn && m_wfn[i].compatible(w2.m_wfn[i]);
     }
-    bool b_vib_basis = (nMode_ == w2.nMode_)&&(nModal_ == w2.nModal_);
+    bool b_vib_basis = (m_nMode == w2.m_nMode) && (m_nModal == w2.m_nModal) && (m_modeCoupling == w2.m_modeCoupling);
     return b_size && b_electronic_wfn && b_vib_basis;
+}
+
+void MixedWavefunction::allocate_buffer() {
+    for(auto & el: m_wfn) el.allocate_buffer();
 }
 
 void MixedWavefunction::axpy(double a, const LinearAlgebra::vector<double> &other) {
     const auto &x = dynamic_cast <const MixedWavefunction &> (other);
-    for (int i = 0; i < wfn_.size(); ++i) {
-        wfn_[i].axpy(a, x.wfn_[i]);
+    for (int i = 0; i < m_wfn.size(); ++i) {
+        m_wfn[i].axpy(a, x.m_wfn[i]);
     }
 }
 
+std::tuple<std::vector<size_t>, std::vector<double> >
+MixedWavefunction::select(const LinearAlgebra::vector<double> &measure,
+                          const size_t maximumNumber = 1000,
+                          const double threshold = 0) const {
+
+}
 
 void MixedWavefunction::scal(double a) {
-    for (auto &el: wfn_) el.scal(a);
-}
-
-double MixedWavefunction::operator*(const MixedWavefunction &w1, const MixedWavefunction &w2) {
-    if (!w1.compatible(w2))
-        throw std::domain_error("attempt to form scalar product between incompatible Wavefunction objects");
-    for (int i = 0; i < wfn_.size(); ++i) {
-        wfn_[i].axpy(a, x.wfn_[i]);
-    }
+    for (auto &el: m_wfn) el.scal(a);
 }
 
 double MixedWavefunction::dot(const LinearAlgebra::vector<double> &other) const {
     return (*this) * ((dynamic_cast<const MixedWavefunction &>(other)));
 }
 
-void MixedWavefunction::times(const LinearAlgebra::vector<double> *a, const LinearAlgebra::vector<double> *b) {
-
-}
-
-void
-MixedWavefunction::divide(const LinearAlgebra::vector<double> *a, const LinearAlgebra::vector<double> *b, double shift,
-                          bool append, bool negative) {
-
-}
-
 void MixedWavefunction::zero() {
-
+    for (auto &el : m_wfn) el.zero();
 }
 
+MixedWavefunction &MixedWavefunction::operator*=(const double &value) {
+    for (auto &el : m_wfn) el *= value;
+    return *this;
+}
+
+MixedWavefunction &MixedWavefunction::operator+=(const MixedWavefunction &other) {
+    if (!compatible(other))
+        throw std::domain_error("Attempting to add incompatible MixedWavefunction objects");
+    for (auto i = 0; i < m_wfn.size(); ++i) m_wfn[i] += other.m_wfn[i];
+    return *this;
+}
+
+MixedWavefunction &MixedWavefunction::operator-=(const MixedWavefunction &other) {
+    if (!compatible(other))
+        throw std::domain_error("Attempting to subtract incompatible MixedWavefunction objects");
+    for (auto i = 0; i < m_wfn.size(); ++i) m_wfn[i] -= other.m_wfn[i];
+    return *this;
+}
+
+MixedWavefunction &MixedWavefunction::operator-=(double value) {
+    for (auto &el: m_wfn) el -= value;
+    return *this;
+}
+
+MixedWavefunction &MixedWavefunction::operator-() {
+    for (auto &el: m_wfn) el = -el;
+    return *this;
+}
+
+MixedWavefunction &MixedWavefunction::operator/=(const MixedWavefunction &other) {
+    if (!compatible(other))
+        throw std::domain_error("Attempting to divide incompatible MixedWavefunction objects");
+    for (auto i = 0; i < m_wfn.size(); ++i) m_wfn[i] /= other.m_wfn[i];
+    return *this;
+}
+
+MixedWavefunction &MixedWavefunction::addAbsPower(const MixedWavefunction &c, double k, double factor) {
+    if (!compatible(c)) throw std::domain_error("MixedWavefunction::addAbsPower vectors must be compatible");
+    for (int i = 0; i < m_wfn.size(); ++i) {
+        m_wfn[i].addAbsPower(c.m_wfn[i], k, factor);
+    }
+    return *this;
+}
+
+void MixedWavefunction::times(const LinearAlgebra::vector<double> *a, const LinearAlgebra::vector<double> *b) {
+    assert(a != nullptr && b != nullptr && "Vectors cannot be null");
+    const auto aa = dynamic_cast<const MixedWavefunction &> (*a);
+    const auto bb = dynamic_cast<const MixedWavefunction &> (*b);
+    assert(compatible(aa) && compatible(bb) && "All vectors must be compatible");
+    for (int i = 0; i < m_wfn.size(); ++i) {
+        m_wfn[i].times(aa.m_wfn[i], bb.m_wfn[i]);
+    }
+}
+
+void MixedWavefunction::divide(const LinearAlgebra::vector<double> *a, const LinearAlgebra::vector<double> *b,
+                               double shift, bool append, bool negative) {
+    assert(a != nullptr && b != nullptr && "Vectors cannot be null");
+    const auto aa = dynamic_cast<const MixedWavefunction &> (*a);
+    const auto bb = dynamic_cast<const MixedWavefunction &> (*b);
+    assert(compatible(aa) && compatible(bb) && "All vectors must be compatible");
+    for (int i = 0; i < m_wfn.size(); ++i) {
+        m_wfn[i].divide(aa.m_wfn[i], bb.m_wfn[i], shift, append, negative);
+    }
+}
+
+double operator*(const MixedWavefunction &w1, const MixedWavefunction &w2) {
+    if (!w1.compatible(w2))
+        throw std::domain_error("Attempt to form scalar product between incompatible MixedWavefunction objects");
+    double result = 0.0;
+    for (int i = 0; i < w1.size(); ++i) {
+        result += w1.m_wfn[i] * w2.m_wfn[i];
+    }
+    return result;
+}
+
+MixedWavefunction operator+(const MixedWavefunction &w1, const MixedWavefunction &w2) {
+    MixedWavefunction result = w1;
+    return result += w2;
+}
+
+MixedWavefunction operator-(const MixedWavefunction &w1, const MixedWavefunction &w2) {
+    MixedWavefunction result = w1;
+    return result -= w2;
+}
+
+MixedWavefunction operator/(const MixedWavefunction &w1, const MixedWavefunction &w2) {
+    MixedWavefunction result = w1;
+    return result /= w2;
+}
+
+MixedWavefunction operator*(const MixedWavefunction &w1, const double &value) {
+    MixedWavefunction result = w1;
+    return result *= value;
+}
+
+MixedWavefunction operator*(const double &value, const MixedWavefunction &w1) {
+    MixedWavefunction result = w1;
+    return result *= value;
+}
+
+}  // namespace gci
--- src/gciMixedWavefunction.h
+++ src/gciMixedWavefunction.h
@@ -6,6 +6,7 @@
 #include <LinearAlgebra.h>
 #include "gciWavefunction.h"
 
+namespace gci {
 /*!
  * @brief Mixed bosonic and fermionic wavefunction represented as configuration expansion in the direct product
  * space of Slater Determenants and Hartree Product of modals. It implements storage of the wavefunction
@@ -52,13 +53,42 @@ public:
      * BBO Hamiltonian determines whether two-mode coupling is required or not in the vibrational basis.
      */
     MixedWavefunction(const State &state, int nMode, int nModal, int modeCoupling = 1);
+    MixedWavefunction(const MixedWavefunction &other, int option) {*this = other;}
+    ~MixedWavefunction() override = default;
 
-    ~MixedWavefunction() = default;
+    size_t size() {return m_dimension;}
+    void allocate_buffer(); //!< allocate buffer to full size
+
+    /*!
+       * \brief find the index of the smallest component
+       * \param n the n'th smallest will be found
+       * \return offset in buffer
+       */
+    size_t minloc(size_t n = 1) const;
+
+    /*!
+       * \brief Get a component of the wavefunction
+       * \param offset which component to get
+       * \return  the value of the component
+       */
+    double at(size_t offset) const;
+
+    /*!
+       * \brief Add to this object the action of an operator on another wavefunction
+       * \param h the operator
+       * \param w the wavefunction
+       * \param parallel_stringset whether to use parallel algorithm in StringSet construction
+       */
+    void operatorOnWavefunction(const gci::Operator &h, const Wavefunction &w, bool parallel_stringset = false);
+
+    void diagonalOperator(const Operator &op); //!< set this object to the diagonal elements of the hamiltonian
 
 private:
-    int nMode_; //!< number of vibrational modes
-    int nModal_; //!< number of modals per vibrational mode
-    int modeCoupling_; //!< level of mode-mode coupling = 1, or 2
+    int m_nMode; //!< number of vibrational modes
+    int m_nModal; //!< number of modals per mode
+    int m_modeCoupling; //!< level of mode-mode coupling = 1, or 2
+    size_t m_elecWfnSize; //!< size of the electronic Wavefunction vector. When we include symmetry each wfn might have different size
+    size_t m_dimension; //!< Overall dimension of the direct product Fock space
 
     /*!
      * @brief Overall wavefunction.
@@ -67,7 +97,7 @@ private:
      * where psi_{p,q} is the electronic CI wavefunction and indices IA imply direct product with HO basis function I of
      * mode A.
      */
-    std::vector<Wavefunction> wfn_;
+    std::vector<Wavefunction> m_wfn;
 public:
     /*!
      * @brief Checks that the two wavefunctions are of the same electronic State and of the same dimension.
@@ -95,21 +125,20 @@ public:
         throw std::logic_error("Cannot assume sparse vector is a map");
     }
 
+    std::tuple<std::vector<size_t>, std::vector<double>>
+    select(const vector<double> &measure, const size_t maximumNumber = 1000,
+           const double threshold = 0) const override {return {{0}, {0}};};
 
     /*!
      * @brief
      * @param a
      */
     void scal(double a) override;
-
     /*!
      * @brief
-     * @param option
+     * @param other
      * @return
      */
-    MixedWavefunction *clone(int option = 0) const override {return new MixedWavefunction(*this, option);}
-
-    double operator*(const MixedWavefunction &w1, const MixedWavefunction &w2);///< inner product of two wavefunctions
     double dot(const LinearAlgebra::vector<double> &other) const override;
 
     double dot(const std::shared_ptr<LinearAlgebra::vector<double> > other) const {
@@ -123,14 +152,71 @@ public:
     double dot(const std::map<size_t, double> &other) const override {
         throw std::logic_error("Cannot assume sparse vector is a map");
     }
+
+    void zero() override;
+
+    /*!
+     * @brief
+     * @param option
+     * @return
+     */
+    MixedWavefunction *clone(int option = 0) const override {return new MixedWavefunction(*this, option);}
+
+    MixedWavefunction &operator*=(const double &value); ///< multiply by a scalar
+    MixedWavefunction &operator+=(const MixedWavefunction &other); ///< add another wavefunction
+    MixedWavefunction &operator-=(const MixedWavefunction &other); ///< subtract another wavefunction
+    MixedWavefunction &operator-=(double); ///< subtract a scalar from every element
+    MixedWavefunction &operator-(); ///< unary minus
+    MixedWavefunction &
+    operator/=(const MixedWavefunction &other); ///< element-by-element division by another wavefunction
+
+    double update(const Wavefunction &diagonalH,
+                  double &eTruncated,
+                  const double dEmax = (double) 0) { }; ///< form a perturbation-theory update, and return the predicted energy change. eTruncated is the energy change lost by truncation
+    /*!
+     * \brief addAbsPower Evaluate this[i] += factor * abs(c[I])^k * c[I]
+     * \param c
+     * \param k
+     * \param factor
+     * \return a pointer to this
+     */
+    MixedWavefunction &addAbsPower(const MixedWavefunction &c, double k = 0, double factor = 1);
+    friend double
+    operator*(const MixedWavefunction &w1, const MixedWavefunction &w2);///< inner product of two wavefunctions
+
+    /*!
+     * \brief this[i] = a[i]*b[i]
+     * \param a
+     * \param b
+     */
     void times(const LinearAlgebra::vector<double> *a, const LinearAlgebra::vector<double> *b);
+    /*!
+     * \brief this[i] = a[i]/(b[i]+shift)
+     * \param a
+     * \param b
+     * \param shift
+     * \param append whether to do += or =
+     * \param negative whether =- or =+
+     */
     void divide(const LinearAlgebra::vector<double> *a,
                 const LinearAlgebra::vector<double> *b,
                 double shift = 0,
                 bool append = false,
                 bool negative = false);
-    void zero() override;
+
+    std::map<std::string, double> m_properties;
+
+    void settilesize(int t = -1, int a = -1, int b = -1) { };
 };
 
+double operator*(const MixedWavefunction &w1, const MixedWavefunction &w2);///< inner product of two wavefunctions
+MixedWavefunction operator+(const MixedWavefunction &w1, const MixedWavefunction &w2); ///< add two wavefunctions
+MixedWavefunction operator-(const MixedWavefunction &w1, const MixedWavefunction &w2); ///< subtract two wavefunctions
+MixedWavefunction operator/(const MixedWavefunction &w1,
+                            const MixedWavefunction &w2); ///< element-by-element division of two wavefunctions
+MixedWavefunction operator*(const MixedWavefunction &w1, const double &value);///< multiply by a scalar
+MixedWavefunction operator*(const double &value, const MixedWavefunction &w1);///< multiply by a scalar
 
+
+}  // namespace gci
 #endif //GCI_GCIMIXEDWAVEFUNCTION_H
