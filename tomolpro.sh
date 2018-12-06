7ab3bdae77be15eccdffddb7ebc65ab36de666e0
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2018-12-06 18:01:52 +0000

    tomolpro

--- tomolpro.sh
+++ tomolpro.sh
@@ -6,16 +6,19 @@ if [ -z "$1" ]; then
 fi
 echo "Synchronize to $1"
 
-rsync src/*.{h,cpp} $1/src/gci
-rsync submodules/memory/memory.h $1/src/global
-rsync submodules/memory/bytestreamC* $1/src/global
-rsync submodules/symmetry_matrix/*.{cpp,h} submodules/symmetry_matrix/SymmetryMatrixF.F90 $1/src/global
-rsync submodules/Profiler/Profiler* $1/src/global
-rsync submodules/FCIdump/FCIdump* $1/src/global
-rsync submodules/IterativeSolver/LinearAlgebra.h $1/src/global
-rsync submodules/IterativeSolver/PagedVector.h $1/src/global
-rsync submodules/IterativeSolver/IterativeSolver.h $1/src/global
-rsync submodules/IterativeSolver/IterativeSolver.cpp $1/src/util
-rsync submodules/IterativeSolver/IterativeSolverF.F90 $1/src/global
-rsync submodules/IterativeSolver/examples/*.{F90,cpp} $1/develop/examples
+rm -f $1/src/global/{SMat,SMatMat,Operator,SMatfunction}.cpp
+rm -f $1/src/gci/gciOperator.*
+rsync src/gci.cpp $1/src/gci
+rsync lib/*.{cpp,h} $1/src/gci
+rsync dependencies/memory/lib/memory.h $1/src/global
+rsync dependencies/memory/lib/bytestreamC* $1/src/global
+rsync dependencies/symmetry_matrix/lib/{SMat,Operator,SMatMat}.h dependencies/symmetry_matrix/lib/SymmetryMatrixF.F90 $1/src/global
+rsync dependencies/symmetry_matrix/lib/{SMat,Operator,SMatMat}-{double.cpp,implementation.h} $1/src/blas
+rsync dependencies/Profiler/lib/Profiler* $1/src/global
+rsync dependencies/FCIdump/lib/FCIdump* $1/src/global
+rsync dependencies/IterativeSolver/lib/PagedVector.h $1/src/global
+rsync dependencies/IterativeSolver/lib/IterativeSolver.h $1/src/global
+rsync dependencies/IterativeSolver/lib/IterativeSolver.cpp $1/src/util
+rsync dependencies/IterativeSolver/lib/IterativeSolverF.F90 $1/src/global
+rsync dependencies/IterativeSolver/example/*.{F90,cpp} $1/develop/examples
 rsync gci.registry $1/lib
