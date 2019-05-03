852a0d4460d4185c6638106d90d1c01af43b1dce
Marat <SibaevM@cardiff.ac.uk>
2019-05-03 10:35:57 +0100

    Indexing bug
    Signed-off-by: Marat <SibaevM@cardiff.ac.uk>

--- lib/gciVibOperator.cpp
+++ lib/gciVibOperator.cpp
@@ -14,25 +14,25 @@ size_t hash(const VibExcitation &exc, int nMode, int nModal, parity_t hermiticit
 }
 
 size_t hash_mc1_sym(const VibExcitation &exc, int nModal) {
-    size_t A = exc.excitations[0][0];
-    size_t n = std::max(exc.excitations[0][1], exc.excitations[0][2]);
-    size_t m = std::min(exc.excitations[0][1], exc.excitations[0][2]);
+    size_t A = 1 + exc.excitations[0][0];
+    size_t n = 1 + std::max(exc.excitations[0][1], exc.excitations[0][2]);
+    size_t m = 1 + std::min(exc.excitations[0][1], exc.excitations[0][2]);
     size_t h_per_mode = nModal * (nModal + 1) / 2;
     return (A - 1) * h_per_mode + n * (n - 1) / 2 + m;
 }
 
 size_t hash_mc1_nosym(const VibExcitation &exc, int nModal) {
-    size_t A = exc.excitations[0][0];
-    size_t i = exc.excitations[0][1];
-    size_t j = exc.excitations[0][2];
+    size_t A = 1 + exc.excitations[0][0];
+    size_t i = 1 + exc.excitations[0][1];
+    size_t j = 1 + exc.excitations[0][2];
     size_t h_per_mode = nModal * nModal;
     return (A - 1) * h_per_mode + (i - 1) * nModal + j;
 }
 
 size_t hash_mc1_nosym_old(const VibExcitation &exc, int nModal) {
-    size_t A = exc.excitations[0][0];
-    size_t i = exc.excitations[0][1];
-    size_t j = exc.excitations[0][2];
+    size_t A = 1 + exc.excitations[0][0];
+    size_t i = 1 + exc.excitations[0][1];
+    size_t j = 1 + exc.excitations[0][2];
     size_t h_per_mode = nModal * nModal;
     if (i >= j) return (A - 1) * h_per_mode + (i - 1) * (i - 1) + j;
     else return (A - 1) * h_per_mode + (j - 1) * (j - 1) + 2 * j - i;
