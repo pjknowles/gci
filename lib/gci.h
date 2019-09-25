963ef886337f2120c5f06fc0feadbd3a492a06a0
Marat <sibaev.marat@gmail.com>
2019-09-25 22:14:55 +0100

    Bug fix
    Signed-off-by: Marat <sibaev.marat@gmail.com>

--- lib/gci.h
+++ lib/gci.h
@@ -62,6 +62,7 @@ extern std::map<MPI_Comm, std::unique_ptr<sharedCounter>> _nextval_counter;
 inline void create_new_counter(MPI_Comm communicator) {
     if (_nextval_counter.find(communicator) == _nextval_counter.end()) {
         _nextval_counter[communicator] = std::make_unique<sharedCounter>(sharedCounter());
+        _nextval_counter[communicator]->reset();
     }
 }
 
--- lib/sharedCounter.cpp
+++ lib/sharedCounter.cpp
@@ -3,58 +3,59 @@
 #include <numeric>
 
 sharedCounter::sharedCounter(const MPI_Comm &communicator)
-    : m_hostrank(0), m_rank(0), m_size(1) {
+        : m_hostrank(0), m_myval(0), m_rank(0), m_size(1) {
 #ifndef SHAREDCOUNTER_DUMMY
-  MPI_Comm_size(communicator, &m_size);
-  MPI_Comm_rank(communicator, &m_rank);
+    MPI_Comm_size(communicator, &m_size);
+    MPI_Comm_rank(communicator, &m_rank);
 #endif
 
-  if (m_rank == m_hostrank) {
-    m_data.assign((size_t) m_size, 0);
+    if (m_rank == m_hostrank) {
+        m_data.assign((size_t) m_size, 0);
 #ifndef SHAREDCOUNTER_DUMMY
-    MPI_Win_create(&m_data[0], m_size * sizeof(int), sizeof(int),
-                   MPI_INFO_NULL, communicator, &m_win);
+        MPI_Win_create(&m_data[0], m_size * sizeof(int), sizeof(int),
+                       MPI_INFO_NULL, communicator, &m_win);
 #endif
-  } else {
+    } else {
 #ifndef SHAREDCOUNTER_DUMMY
-    MPI_Win_create(&m_data[0], 0, sizeof(int),
-                   MPI_INFO_NULL, communicator, &m_win);
+        MPI_Win_create(&m_data[0], 0, sizeof(int),
+                       MPI_INFO_NULL, communicator, &m_win);
 #endif
-  }
+    }
 }
+
 sharedCounter::~sharedCounter() {
 #ifndef SHAREDCOUNTER_DUMMY
-  MPI_Win_free(&m_win);
+    MPI_Win_free(&m_win);
 #endif
 }
 
 void sharedCounter::reset() {
-  m_myval = 0;
-  if (m_rank == m_hostrank)
-    m_data.assign((size_t) m_size, 0);
+    m_myval = 0;
+    if (m_rank == m_hostrank)
+        m_data.assign((size_t) m_size, 0);
 }
 
-#include <iostream>
+
 int sharedCounter::increment(int amount) {
-  std::vector<int> vals(static_cast<unsigned long>(m_size));
+    std::vector<int> vals(static_cast<unsigned long>(m_size));
 
 #ifndef SHAREDCOUNTER_DUMMY
-  MPI_Win_lock(MPI_LOCK_EXCLUSIVE, m_hostrank, 0, m_win);
+    MPI_Win_lock(MPI_LOCK_EXCLUSIVE, m_hostrank, 0, m_win);
 
-  for (int i = 0; i < m_size; i++) {
-    if (i == m_rank) {
-      MPI_Accumulate(&amount, 1, MPI_INT, 0, i, 1, MPI_INT, MPI_SUM,
-                     m_win);
-    } else {
-      MPI_Get(&vals[i], 1, MPI_INT, 0, i, 1, MPI_INT, m_win);
+    for (int i = 0; i < m_size; i++) {
+        if (i == m_rank) {
+            MPI_Accumulate(&amount, 1, MPI_INT, 0, i, 1, MPI_INT, MPI_SUM,
+                           m_win);
+        } else {
+            MPI_Get(&vals[i], 1, MPI_INT, 0, i, 1, MPI_INT, m_win);
+        }
     }
-  }
 
-  MPI_Win_unlock(0, m_win);
+    MPI_Win_unlock(0, m_win);
 #endif
 
-  vals[m_rank] = m_myval;
-  m_myval += amount;
+    vals[m_rank] = m_myval;
+    m_myval += amount;
 //  std::cout << "vals[m_rank], m_myval"<<vals[m_rank]<<", "<<m_myval<<std::endl;
-  return std::accumulate(vals.begin(), vals.end(), 0); // first returned value is zero
+    return std::accumulate(vals.begin(), vals.end(), 0); // first returned value is zero
 }
