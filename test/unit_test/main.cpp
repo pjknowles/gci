23a90fd5a22da03eeacf35f7b8fd983f12aef12a
Marat <sibaev.marat@gmail.com>
2019-10-08 14:25:03 +0100

    Array unit tests passing with GA using MPI3 communication protocol

--- test/unit_test/main.cpp
+++ test/unit_test/main.cpp
@@ -1,21 +1,25 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <gci.h>
+#include <ma.h>
 #include <SharedCounter.h>
 
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    int result;
-    gci::profiler = std::make_unique<Profiler>(Profiler("GCI"));
     MPI_Init(&argc, &argv);
     GA_Initialize();
+    int mem = 10000000;
+    MA_init(C_DBL, mem, mem);
     gci::mpi_comm_compute = GA_MPI_Comm();
     MPI_Comm_rank(gci::mpi_comm_compute, &gci::parallel_rank);
     MPI_Comm_size(gci::mpi_comm_compute, &gci::parallel_size);
-    result = RUN_ALL_TESTS();
-    gci::profiler.reset();
+    gci::profiler = std::make_unique<Profiler>(Profiler("GCI"));
     gci::_nextval_counter[gci::mpi_comm_compute].reset(nullptr);
+    if (!GA_Create_mutexes(1)) GA_Error((char *) "Failed to create mutexes", 1);
+    int result = RUN_ALL_TESTS();
+    GA_Destroy_mutexes();
+    gci::profiler.reset();
     GA_Terminate();
     MPI_Finalize();
     return result;
