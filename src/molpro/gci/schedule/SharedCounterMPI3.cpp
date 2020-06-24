904a75c2de1f325152e0dc9516f5ca80e0548370
Marat Sibaev <sibaev.marat@gmail.com>
2020-06-24 18:44:07 +0100

    fixed type mismatch

--- src/molpro/gci/schedule/SharedCounterMPI3.cpp
+++ src/molpro/gci/schedule/SharedCounterMPI3.cpp
@@ -1,4 +1,5 @@
 #include "SharedCounterMPI3.h"
+#include <iostream>
 
 namespace molpro::gci::schedule {
 SharedCounterMPI3::SharedCounterMPI3(const MPI_Comm &communicator, int target_rank)
@@ -11,6 +12,7 @@ SharedCounterMPI3::SharedCounterMPI3(const MPI_Comm &communicator, int target_ra
   MPI_Aint n = m_rank == target_rank ? 1 : 0;
   auto sizeoftype = sizeof(unsigned long int);
   MPI_Win_allocate(n * sizeoftype, sizeoftype, MPI_INFO_NULL, m_communicator, &m_win_buffer, &m_win);
+  SharedCounterMPI3::reset();
 }
 
 SharedCounterMPI3::~SharedCounterMPI3() {
@@ -29,7 +31,8 @@ void SharedCounterMPI3::reset() {
 
 unsigned long int SharedCounterMPI3::increment(int amount) {
   MPI_Win_lock(MPI_LOCK_SHARED, m_target_rank, 0, m_win);
-  MPI_Fetch_and_op(&amount, &m_curr_val, MPI_UNSIGNED_LONG, m_target_rank, 0, MPI_SUM, m_win);
+  unsigned long int a = amount;
+  MPI_Fetch_and_op(&a, &m_curr_val, MPI_UNSIGNED_LONG, m_target_rank, 0, MPI_SUM, m_win);
   MPI_Win_unlock(m_target_rank, m_win);
   return m_curr_val;
 }
