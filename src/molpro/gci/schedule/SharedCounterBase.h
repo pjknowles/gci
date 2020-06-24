aa0795fbe0e21a82ad43137425fe5fe51b620a59
Marat Sibaev <sibaev.marat@gmail.com>
2020-06-24 17:33:47 +0100

    defined SharedCounterBase

--- src/molpro/gci/schedule/SharedCounterBase.h
+++ src/molpro/gci/schedule/SharedCounterBase.h
@@ -1,19 +1,29 @@
 #ifndef GCI_SRC_MOLPRO_GCI_SCHEDULE_SHAREDCOUNTERBASE_H
 #define GCI_SRC_MOLPRO_GCI_SCHEDULE_SHAREDCOUNTERBASE_H
 
-namespace molpro {
-namespace gci {
-namespace schedule {
-
+namespace molpro::gci::schedule {
 /*!
  * @brief Base class for shared counters defining the interface.
  */
 class SharedCounterBase {
-  SharedCounterBase()=delete;
+public:
+  SharedCounterBase() = default;
+  SharedCounterBase(const SharedCounterBase &) = delete;
+  virtual ~SharedCounterBase() = default;
+
+  /*!
+   * @brief increment the counter
+   * @param amount how much to increment by
+   * @return current value of the counter
+   */
+  virtual int increment(int amount = 1) = 0;
+
+  /*!
+   * @brief reset counter value to 0
+   */
+  virtual void reset() = 0;
 };
 
-} // namespace schedule
-} // namespace gci
-} // namespace molpro
+}; // namespace molpro::gci::schedule
 
 #endif // GCI_SRC_MOLPRO_GCI_SCHEDULE_SHAREDCOUNTERBASE_H
