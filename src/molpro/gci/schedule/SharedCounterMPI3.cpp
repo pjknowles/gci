23d3cd1433d208b7bf03ea3eb9fed9175252dd52
Marat Sibaev <sibaev.marat@gmail.com>
2020-06-24 18:08:52 +0100

    updated SharedCounterMPI3

--- src/molpro/gci/schedule/SharedCounterMPI3.cpp
+++ src/molpro/gci/schedule/SharedCounterMPI3.cpp
@@ -1,9 +1,8 @@
 #include "SharedCounterMPI3.h"
 
-namespace molpro {
-namespace gci {
+namespace molpro::gci::schedule {
 SharedCounterMPI3::SharedCounterMPI3(const MPI_Comm &communicator, int target_rank)
-    : m_communicator(communicator), m_myval(0), m_rank(0), m_target_rank(target_rank) {
+    : m_communicator(communicator), m_rank(0), m_target_rank(target_rank) {
   int size;
   MPI_Comm_size(m_communicator, &size);
   MPI_Comm_rank(m_communicator, &m_rank);
@@ -12,15 +11,6 @@ SharedCounterMPI3::SharedCounterMPI3(const MPI_Comm &communicator, int target_ra
   MPI_Aint n = m_rank == target_rank ? 1 : 0;
   auto sizeoftype = sizeof(unsigned long int);
   MPI_Win_allocate(n * sizeoftype, sizeoftype, MPI_INFO_NULL, m_communicator, &m_win_buffer, &m_win);
-  reset();
-}
-
-std::map<MPI_Comm, std::shared_ptr<SharedCounterMPI3>> SharedCounterMPI3::m_counters{};
-
-std::shared_ptr<SharedCounterMPI3> SharedCounterMPI3::instance(const MPI_Comm &communicator) {
-  if (!m_counters.count(communicator))
-    m_counters.insert({communicator, std::make_shared<SharedCounterMPI3>(communicator)});
-  return m_counters[communicator];
 }
 
 SharedCounterMPI3::~SharedCounterMPI3() {
@@ -31,21 +21,16 @@ SharedCounterMPI3::~SharedCounterMPI3() {
 
 void SharedCounterMPI3::reset() {
   MPI_Win_fence(0, m_win);
-  m_myval = 0;
+  m_curr_val = 0;
   if (m_rank == m_target_rank)
     *m_win_buffer = 0;
   MPI_Win_fence(0, m_win);
 }
 
-unsigned long int SharedCounterMPI3::myval() { return m_myval; }
-
-unsigned long int SharedCounterMPI3::increment(unsigned long int amount) {
-  m_myval += amount;
+unsigned long int SharedCounterMPI3::increment(int amount) {
   MPI_Win_lock(MPI_LOCK_SHARED, m_target_rank, 0, m_win);
-  unsigned long int glob_val;
-  MPI_Fetch_and_op(&amount, &glob_val, MPI_UNSIGNED_LONG, m_target_rank, 0, MPI_SUM, m_win);
+  MPI_Fetch_and_op(&amount, &m_curr_val, MPI_UNSIGNED_LONG, m_target_rank, 0, MPI_SUM, m_win);
   MPI_Win_unlock(m_target_rank, m_win);
-  return glob_val;
+  return m_curr_val;
 }
-} // namespace gci
-} // namespace molpro
+} // namespace molpro::gci::schedule
