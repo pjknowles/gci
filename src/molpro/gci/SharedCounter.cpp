f1e8163c4a11084b6578355297e443e0f1aa3f84
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2020-06-24 07:24:29 +0100

    SharedCounter: do not create GA if MPI size is only 1
    ... because when running under PPIDD, GA is switched off if serial.

--- src/molpro/gci/SharedCounter.cpp
+++ src/molpro/gci/SharedCounter.cpp
@@ -4,7 +4,7 @@
 
 namespace molpro {
 namespace gci {
-SharedCounter::SharedCounter(const MPI_Comm &communicator)
+SharedCounter::SharedCounter(const MPI_Comm& communicator)
     : m_communicator(communicator), m_hostrank(0), m_myval(0), m_ga_handle(0), m_rank(0), m_size(1) {
 #ifndef SHAREDCOUNTER_DUMMY
   int glob_size, glob_rank;
@@ -15,32 +15,37 @@ SharedCounter::SharedCounter(const MPI_Comm &communicator)
   auto glob_ranks = std::vector<int>(m_size, 0);
   MPI_Allgather(&glob_rank, 1, MPI_INT, glob_ranks.data(), 1, MPI_INT, m_communicator);
   // create new processor group from the communicator
-  m_ga_pgroup = GA_Pgroup_create(glob_ranks.data(), m_size);
-  m_ga_handle = NGA_Create_handle();
-  NGA_Set_pgroup(m_ga_handle, m_ga_pgroup);
-  int dims = 1, chunk = 1;
-  NGA_Set_data(m_ga_handle, 1, &dims, C_LONG);
-  NGA_Set_array_name(m_ga_handle, (char *)"SharedCounter");
-  NGA_Set_chunk(m_ga_handle, &chunk);
-  auto succ = GA_Allocate(m_ga_handle);
-  if (!succ)
-    GA_Error((char *)"Failed to allocate", 0);
-  GA_Check_handle(m_ga_handle, (char *)"Failed in SharedCounter constructor");
+  if (m_size > 1) {
+    m_ga_pgroup = GA_Pgroup_create(glob_ranks.data(), m_size);
+    m_ga_handle = NGA_Create_handle();
+    NGA_Set_pgroup(m_ga_handle, m_ga_pgroup);
+    int dims = 1, chunk = 1;
+    NGA_Set_data(m_ga_handle, 1, &dims, C_LONG);
+    NGA_Set_array_name(m_ga_handle, (char*) "SharedCounter");
+    NGA_Set_chunk(m_ga_handle, &chunk);
+    auto succ = GA_Allocate(m_ga_handle);
+    if (!succ)
+      GA_Error((char*) "Failed to allocate", 0);
+    GA_Check_handle(m_ga_handle, (char*) "Failed in SharedCounter constructor");
+  }
   reset();
 #endif
 }
 
 SharedCounter::~SharedCounter() {
 #ifndef SHAREDCOUNTER_DUMMY
-  GA_Destroy(m_ga_handle);
+  if (m_size > 1)
+    GA_Destroy(m_ga_handle);
 #endif
 }
 
 void SharedCounter::reset() {
   m_myval = 0;
 #ifndef SHAREDCOUNTER_DUMMY
-  GA_Check_handle(m_ga_handle, (char *)"Failed in SharedCounter::reset()");
-  GA_Zero(m_ga_handle);
+  if (m_size > 1) {
+    GA_Check_handle(m_ga_handle, (char*) "Failed in SharedCounter::reset()");
+    GA_Zero(m_ga_handle);
+  }
 #endif
 }
 
@@ -48,8 +53,10 @@ int SharedCounter::increment(int amount) {
   auto glob_val = m_myval;
   m_myval += amount;
 #ifndef SHAREDCOUNTER_DUMMY
-  int subscript = 0;
-  glob_val = NGA_Read_inc(m_ga_handle, &subscript, (long int)amount);
+  if (m_size > 1) {
+    int subscript = 0;
+    glob_val = NGA_Read_inc(m_ga_handle, &subscript, (long int) amount);
+  }
 #endif
   return glob_val;
 }
