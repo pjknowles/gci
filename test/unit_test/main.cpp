59d0d45ca8b60aa2f05b0d9121e166f53d8582bf
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2021-04-27 09:26:04 +0100

    Use WeakSingleton Profiler so that libraries using it too will get profiled here

--- test/unit_test/main.cpp
+++ test/unit_test/main.cpp
@@ -3,6 +3,7 @@
 #include <macdecls.h>
 #include <molpro/gci/gci.h>
 #include <molpro/gci/schedule/SharedCounterGA.h>
+#include <molpro/Profiler.h>
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
@@ -13,13 +14,11 @@ int main(int argc, char **argv) {
   molpro::gci::mpi_comm_compute = GA_MPI_Comm();
   MPI_Comm_rank(molpro::gci::mpi_comm_compute, &molpro::gci::parallel_rank);
   MPI_Comm_size(molpro::gci::mpi_comm_compute, &molpro::gci::parallel_size);
-  molpro::gci::profiler = std::make_unique<molpro::Profiler>(molpro::Profiler("GCI"));
   molpro::gci::_nextval_counter[molpro::gci::mpi_comm_compute].reset(nullptr);
   if (!GA_Create_mutexes(1))
     GA_Error((char *)"Failed to create mutexes", 1);
   int result = RUN_ALL_TESTS();
   GA_Destroy_mutexes();
-  molpro::gci::profiler.reset();
   GA_Terminate();
   MPI_Finalize();
   return result;
